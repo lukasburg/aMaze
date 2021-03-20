98609e8db0becc42c415554644777a1fe4ff5483
lukas <lukiburg@web.de>
2021-03-20 20:46:08 +0100

    Fix relative key file path

--- aMazeTogether/settings.py
+++ aMazeTogether/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-with open('secret_key.txt') as key_file:
+with open(os.path.join(BASE_DIR, 'secret_key.txt')) as key_file:
     SECRET_KEY = key_file.readline().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
